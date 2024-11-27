639aa5440d264002b6f61aca585f22c3d8e10b1c
Adewole Ridwan <devridwan1@gmail.com>
2024-11-27 20:49:43 +0000

    update

--- src/pages/components/ThrowbackSection.tsx
+++ src/pages/components/ThrowbackSection.tsx
@@ -6,11 +6,11 @@ import { Play, Image } from "lucide-react";
 
 function ThrowbackSection({ data }: { data: any }) {
   return (
-<!-- <<<<<<< hero-section-changes -->
+// <!-- <<<<<<< hero-section-changes -->
     <section className="py-24 lg:pb-24 bg-black text-white">
-=======
+{/* =======
 <!--     <section className="relative pt-24 bg-blue text-white"> -->
-<!-- >>>>>>> main -->
+<!-- >>>>>>> main --> */}
       <div className="container mx-auto px-4">
         <div className="max-w-4xl mx-auto">
           <motion.div
