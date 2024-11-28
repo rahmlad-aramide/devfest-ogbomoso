1575940e5567591562de0baecc3c25737d4a502e
Oluwaferanmi Adeniji <feranmidev@gmail.com>
2024-11-28 03:49:25 +0100

    fix

--- src/pages/components/StepForm/Steps/RefferalCode.tsx
+++ src/pages/components/StepForm/Steps/RefferalCode.tsx
@@ -1,8 +1,18 @@
 import { motion } from 'framer-motion';
 import { StepProps } from '../types';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export function RefferalCodeStep({ formData, updateFields, onNext }: StepProps) {
+  const [hydrated, setHydrated] = useState(false);
+  useEffect(() => {
+    // this forces a rerender
+    setHydrated(true);
+  }, []);
+
+  if (!hydrated) {
+    // this returns null on first render, so the client and server match
+    return null;
+  }
   // get url search param code to prefilled refferal code
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get('code');
